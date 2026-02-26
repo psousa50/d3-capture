bb807ae2af4bde404809e1819b1bb0220159d0c9
Pedro Sousa <pedronsousa@gmail.com>
2026-02-26 17:21:47 +0000

    remove pino-pretty transport so logs pipe correctly to files

--- server/logger.ts
+++ server/logger.ts
@@ -2,8 +2,4 @@ import pino from "pino";
 
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
-  transport:
-    process.env.NODE_ENV !== "production"
-      ? { target: "pino-pretty" }
-      : undefined,
 });
